6b225adf16f9be876961b08899d7e5b1d2627def
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2023-06-26 11:21:28 +0200

    a bit more readable names, comments

--- f_score.py
+++ f_score.py
@@ -7,21 +7,19 @@ spm.SentencePieceTrainer.Train('--input=bochan.txt --model_prefix=sentencepiece 
 sp=spm.SentencePieceProcessor()
 sp.Load("sentencepiece.model")
 
-def calculate_f_score(corpus, output_text):
+def calculate_f_score(reference, output_text):
     # Calculate precision, recall, and F-score
-    precision = nltk.precision(set(corpus), set(output_text))
-    recall = nltk.recall(set(corpus), set(output_text))
-    f_score = nltk.f_measure(set(corpus), set(output_text))
+    precision = nltk.precision(reference, output_text)
+    recall = nltk.recall(reference, output_text)
+    f_score = nltk.f_measure(reference, output_text)
     print("Precision:", precision)
     print("Recall:", recall)
     print("F-score:", f_score)
     #return precision, recall, f_score
 
-with open('non_notation.txt') as f, open('full_annotation.txt') as f1:
-    text = f.read()
-    text1 = f1.read().split()   # split on spaces!
-    a = len(text)
-    b = sp.EncodeAsPieces(text)  
-    calculate_f_score(b, text1)
-    f.close()
-    f1.close()
+with open('non_notation.txt') as f_in, open('full_annotation.txt') as f_ref:
+    text_in = f_in.read()
+    reference = f_ref.read().split()    # split into a list
+    output = sp.EncodeAsPieces(text_in) # sp already returns a list
+    calculate_f_score(set(reference),
+                      set(output))
--- mecab_score.py
+++ mecab_score.py
@@ -4,23 +4,21 @@ import MeCab
 #nltk.download("punkt")
 from nltk.metrics import precision, recall, f_measure
 
-def calculate_f_score(corpus, output_text):
+wakati = MeCab.Tagger("-Owakati")
+
+def calculate_f_score(reference, output_text):
     # Calculate precision, recall, and F-score
-    precision = nltk.precision(set(corpus), set(output_text))
-    recall = nltk.recall(set(corpus), set(output_text))
-    f_score = nltk.f_measure(set(corpus), set(output_text))
+    precision = nltk.precision(reference, output_text)
+    recall = nltk.recall(reference, output_text)
+    f_score = nltk.f_measure(reference, output_text)
     print("Precision:", precision)
     print("Recall:", recall)
     print("F-score:", f_score)
     #return precision, recall, f_score
 
-with open('non_notation.txt') as f, open('full_annotation.txt') as f1:
-    text = f.read()
-    text1 = f1.read().split()   # split on spaces!
-    a = len(text)
-    wakati = MeCab.Tagger("-Owakati")
-    b = wakati.parse(text).split()  
-    print(b)
-    calculate_f_score(b, text1)
-    f.close()
-    f1.close()
+with open('non_notation.txt') as f_in, open('full_annotation.txt') as f_ref:
+    text_in = f_in.read()
+    reference = f_ref.read().split()       # split into a list
+    output = wakati.parse(text_in).split() # split into a list
+    calculate_f_score(set(reference),
+                      set(output))
