f1cfa9b7cbcaad32d5712ed92fde0c7b90e2c2c6
Pompokopom5522 <99264752+yypy22@users.noreply.github.com>
2023-07-08 05:26:22 +0100

    tokenizer with mecab and word segmentation seems working

--- tokenize_mecab.py
+++ tokenize_mecab.py
@@ -1,56 +1,31 @@
-import MeCab 
-import hfst
-from itertools import product
+import MeCab
+import sys
 
-mecab_tokenizer = MeCab.Tagger("-Owakati")
-
-def tokenizer_mecab(sin, sout):
-    if sin is None:
-        return ""
-    else:
-        sout.write(mecab_tokenizer.parse(sin))
-        return sout
-
-def analyzer_mecab(sin, sout):
-    if sin is None:
-        return ""
-    else:
-        lines = ""
-        sout.write(mecab_tokenizer.parse(sin))
-        # for i in range(len(sout)):
-        #     lines += (sout[i] + "^")
-        return lines
-
-def process_stream(tokenizer, sin, sout, analyzer=None):
-    alpha = tokenizer.get_alphabet()
-    cur_word = ''
-    while True:
-        c = sin.read(1)
-        if c in alpha:
-            cur_word += c
-        else:
-            if cur_word:
-                if analyzer:
-                    analyzer_mecab(cur_word, sout)
-                else:
-                    tokenizer_mecab(cur_word, sout)
-            if c:
-                cur_word = ''
-                sout.write(c)
-            else:
-                break
+def go_to_tokenize(sin, sout):
+    text = sin.read()
+    excluded = []
+    current = ""
+    in_blank = False
+    for i in text:
+        if i == "[":
+            current = ""
+            in_blank = True
+        elif i == "]":
+            current += i
+            in_blank = False
+            excluded.append(current)
+            current = ""
+        if in_blank:
+            current += i
+    ##Alternative would be change setting in mecab dictionary
+    ##like dont process []
+    for i in range(len(excluded)):
+        text = text.replace(excluded[i], "^"+str(i)+"^")
+    mecab = MeCab.Tagger("-Owakati")
+    token = mecab.parse(text)
+    for i in range(len(excluded)):
+        token = token.replace("^"+" "+str(i)+" "+"^", excluded[i])
+    sout.write(token)
 
 if __name__ == '__main__':
-    import argparse
-    prs = argparse.ArgumentParser(description='Segment input stream using HFST')
-    prs.add_argument('transducer')
-    prs.add_argument('analyzer', nargs='?')
-    args = prs.parse_args()
-    stream_tok = hfst.HfstInputStream(args.transducer)
-    tokenizer = hfst.HfstBasicTransducer(stream_tok.read())
-    analyzer = None
-    if args.analyzer:
-        stream_morf = hfst.HfstInputStream(args.analyzer)
-        analyzer = hfst.HfstBasicTransducer(stream_morf.read())
-    import sys
-    process_stream(tokenizer, sys.stdin, sys.stdout, analyzer)
+    go_to_tokenize(sys.stdin, sys.stdout)
