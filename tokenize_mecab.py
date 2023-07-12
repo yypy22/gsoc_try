dd8bbce36f0cbfca07a2bac1566493202461b9b7
Pompokopom5522 <99264752+yypy22@users.noreply.github.com>
2023-07-12 13:20:45 +0100

    from tokenize.py on apertium-jpn

--- tokenize_mecab.py
+++ tokenize_mecab.py
@@ -10,16 +10,23 @@ def process_text(sin, sout):
 
     for i in text:
         buffer += i
+        if i == text[-1]:
+            buffer = mecab.parse(buffer.strip()).rstrip()
+            tokenized += buffer
+
         if i == "[":
+            buffer = buffer[:-1]
             buffer = mecab.parse(buffer.strip()).rstrip()
             tokenized += buffer
             buffer = ""
             in_blancket = True
-        elif in_blancket:
-            tokenized += i
-        elif i == "]":
-            in_blancket = False
-    sout.write(tokenized)
 
+        if in_blancket:
+            tokenized += i
+
+        if i == "]":
+            in_blancket = False
+            buffer = ""
+    sout.write(tokenized)
 if __name__ == '__main__':
     process_text(sys.stdin, sys.stdout)
